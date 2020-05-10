e5b60fd1615e925a64bb7489a17a4ac8af433f1a
Kenechukwu Nnaji <P7849@fidelitybank.ng>
2020-05-10 10:24:04 +0100

    Updated Message

--- Startup.cs
+++ Startup.cs
@@ -32,7 +32,7 @@ namespace azuredemo1
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    await context.Response.WriteAsync("Hello Azure!!!");
                 });
             });
         }
